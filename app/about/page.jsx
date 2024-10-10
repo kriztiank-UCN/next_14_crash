26c3b7aeed5450f95ff310a88496ab73f1811c88
Kristian <10221425@ucn.dk>
2024-10-10 07:18:40 +0200

    Routing System

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>AboutPage</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea maiores beatae laboriosam quod,
+        nostrum quo exercitationem quas ipsa ad! Totam cum accusantium vero ab, expedita ea
+        voluptatum sit aperiam aspernatur.
+      </p>
+    </div>
+  )
+}
+export default AboutPage
