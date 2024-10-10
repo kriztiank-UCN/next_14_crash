1ffe58a44cbf99fc65ef80d4c1a7d7ce759564cb
Kristian <10221425@ucn.dk>
2024-10-10 08:06:13 +0200

    Nested Routes

---
+++ app/about/team/page.jsx
@@ -0,0 +1,13 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>TeamPage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit optio officia accusamus
+        voluptate delectus vero quibusdam nisi totam ex ab ipsa autem nobis modi numquam fuga,
+        explicabo ratione aliquam quidem.
+      </p>
+    </div>
+  )
+}
+export default TeamPage
