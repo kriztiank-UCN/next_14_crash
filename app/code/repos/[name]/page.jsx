2ab3673acec75a59c11c841f9f5e354f6ac8058f
Kristian <10221425@ucn.dk>
2024-10-12 10:27:18 +0200

    Suspense Boundaries

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,6 +1,7 @@
 import Repo from "@/app/components/Repo"
 import RepoDirs from "@/app/components/RepoDirs"
 import Link from "next/link"
+import { Suspense } from "react"
 
 const RepoPage = ({ params: { name } }) => {
   return (
@@ -8,9 +9,13 @@ const RepoPage = ({ params: { name } }) => {
       <Link href="/code/repos" className="btn btn-back">
         Back to Repositories
       </Link>
-      {/* pass the name which comes from the URL to the Repo component */}
-      <Repo name={name} />
-      <RepoDirs name={name} />
+      {/* pass the name which comes from the URL to the Repo & RepoDirs components */}
+      <Suspense fallback={<div>Loading repo...</div>}>
+        <Repo name={name} />
+      </Suspense>
+      <Suspense fallback={<div>Loading directories...</div>}>
+        <RepoDirs name={name} />
+      </Suspense>
     </div>
   )
 }
