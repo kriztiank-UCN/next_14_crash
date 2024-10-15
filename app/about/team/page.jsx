e120cf0eb8a0496a0f1fd10b9eafd5222c73d734
kk <kk@softnotik.dk>
2024-10-15 11:00:48 +0200

    Serving & Fetching Course Data

--- app/about/team/page.jsx
+++ app/about/team/page.jsx
@@ -1,7 +1,7 @@
 const TeamPage = () => {
   return (
     <div>
-      <h1>TeamPage</h1>
+      <h1>Team Page</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit optio officia accusamus
         voluptate delectus vero quibusdam nisi totam ex ab ipsa autem nobis modi numquam fuga,
--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,11 @@
-import Link from "next/link"
+import Courses from "./components/Courses"
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to the HomePage</h1>
-      <ul>
-        <li><Link href='/'>Home</Link> </li>
-        <li><Link href='/about'>About</Link> </li>
-        <li><Link href='/about/team'>Team</Link> </li>
-      </ul>
-    </div>
+    <>
+      <h1>Welcome to Traversy Media</h1>
+      <Courses />
+    </>
   )
 }
 export default HomePage
